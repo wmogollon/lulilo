16e53f9a9b2df7689cd1140ccbfe475dbca26736
wmogollon <wmogollon89@gmail.com>
2026-06-29 19:05:27 -0400

    Delete lib directory

--- lib/providers.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { OrderProvider } from "@/lib/order-context";
-import { ReactNode } from "react";
-
-export default function Providers({ children }: { children: ReactNode }) {
-  return <OrderProvider>{children}</OrderProvider>;
-}
